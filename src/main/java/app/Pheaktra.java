b5c0996b49e4f279cf23fd6be6b4212a7867a0a9
bosnhta <pheaktra0017@gmail.com>
2024-10-26 23:09:01 +0700

    Pheaktra(p11)

--- src/main/java/app/Pheaktra.java
+++ src/main/java/app/Pheaktra.java
@@ -4,26 +4,10 @@ public class
 Pheaktra {
     public static void practise()
     {
-        //p10
-        int a = 6;
-        int b = 3;
-
-        int and = a & b;
-        System.out.println("a & b   : " + and);
-
-        int or = a | b;
-        System.out.println("a | b   : " + or);
-
-        int xor = a ^ b;
-        System.out.println("a ^ b   : " + xor);
-
-        int not = ~a;
-        System.out.println("~a      : " + not);
-
-        int leftShift = a << 1;
-        System.out.println("a << 1  : " + leftShift);
-
-        int rightShift = a >> 1;
-        System.out.println("a >> 1  : " + rightShift);
+        //p11
+        int x = 8;
+        int y = 10;
+        int greater = (x > y) ? x : y;
+        System.out.println("Greater value   : " + greater);
     }
 }
