0776a6e735fd21c2bae9980d255ca8f7db197e2d
bosnhta <pheaktra0017@gmail.com>
2024-10-26 22:35:53 +0700

    Pheaktra(p8)

--- src/main/java/app/Pheaktra.java
+++ src/main/java/app/Pheaktra.java
@@ -4,7 +4,7 @@ public class
 Pheaktra {
     public static void practise()
     {
-        //p7
+        //p8
         int x = 5;
         int y = 10;
         System.out.println("x > 0 && y < 15  : " + (x > 0 && y < 15));
