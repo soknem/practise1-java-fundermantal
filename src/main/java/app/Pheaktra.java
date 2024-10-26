126b7da96ebe6cb29bf979011b050c5b46808731
bosnhta <pheaktra0017@gmail.com>
2024-10-26 22:12:25 +0700

    Pheaktra(p3)

--- src/main/java/app/Pheaktra.java
+++ src/main/java/app/Pheaktra.java
@@ -4,13 +4,14 @@ public class
 Pheaktra {
     public static void practise()
     {
-        //p2
-        double a2 = 9.99;
-        int b2    = (int) a2;
-        System.out.println("a2 (9.99) cast to int : " + b2);
+        //p3
+        int a3     = 10;
+        Integer b3 = a3;
+        System.out.println("a3 (10) autoboxed to Integer  : " + b3);
 
-        float c2 = 5.75f;
-        long d2  = (long) c2;
-        System.out.println("c2 (5.75) cast to long: " + d2);
+        double c3 = 12.34;
+        Double d3 = c3;
+        System.out.println("c3 (12.34) autoboxed to Double: " + d3);
+
     }
 }
