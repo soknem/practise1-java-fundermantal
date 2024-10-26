4a4cd8a306d6aba16b7340ab1af600f0b435c8fa
bosnhta <pheaktra0017@gmail.com>
2024-10-26 22:16:58 +0700

    Pheaktra(p3)

--- src/main/java/app/Pheaktra.java
+++ src/main/java/app/Pheaktra.java
@@ -4,14 +4,15 @@ public class
 Pheaktra {
     public static void practise()
     {
-        //p3
-        int a3     = 10;
-        Integer b3 = a3;
-        System.out.println("a3 (10) autoboxed to Integer  : " + b3);
+        //p4
+        Integer a4 = 25;
+        int b4 = a4;
+        System.out.println("a4 (25) unboxed to int      : " + b4);
 
-        double c3 = 12.34;
-        Double d3 = c3;
-        System.out.println("c3 (12.34) autoboxed to Double: " + d3);
+        Double c4 = 56.78;
+        double d4 = c4;
+        System.out.println("c4 (56.78) unboxed to double: " + d4);
+
 
     }
 }
