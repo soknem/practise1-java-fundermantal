3962b303f6a60a8d4d1d6215e8bae079e87f7dda
bosnhta <pheaktra0017@gmail.com>
2024-10-26 23:17:05 +0700

    Pheaktra(p13)

--- src/main/java/app/Pheaktra.java
+++ src/main/java/app/Pheaktra.java
@@ -1,14 +1,18 @@
 package app;
-
+import java.util.Scanner;
 public class
 Pheaktra {
+
     public static void practise()
     {
-        //p12
-        String firstName = "John";
-        String lastName  = "Doe";
-        String fullName  = firstName + " " + lastName;
-        System.out.println("Full Name   : " + fullName);
+        //p13
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name   : ");
+        String name     = scanner.nextLine();
+        System.out.print("Enter your age    : ");
+        int age         = scanner.nextInt();
+        System.out.println("Hello, " + name + "! You are " + age + " years old.");
+        scanner.close();
 
     }
 }
