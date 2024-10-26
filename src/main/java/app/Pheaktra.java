43147968c27ebca80edfcc25f4e01d383e62b374
bosnhta <pheaktra0017@gmail.com>
2024-10-26 21:49:36 +0700

    Pheaktra(p1)

--- src/main/java/app/Pheaktra.java
+++ src/main/java/app/Pheaktra.java
@@ -2,7 +2,45 @@ package app;
 
 public class
 Pheaktra {
-    public static void practise(){
-        System.out.println("hello pheaktra");
+    public static void practise()
+    {
+        int a     = 10;
+        float b   = 3.14f;
+        char c    = 'A';
+        boolean d = true;
+        double e  = 9.876;
+        long f    = 1000000000L;
+        byte g    = 20;
+        short h   = 300;
+
+        Integer a1   = 15;
+        Float b1     = 2.71f;
+        Character c1 = 'B';
+        Boolean e1   = false;
+        Double f1    = 1.2345;
+        Long g1      = 2000000000L;
+        Byte h1      = 30;
+        Short i1     = 400;
+
+        System.out.println("Primitive Types:");
+        System.out.println("int    : " + a);
+        System.out.println("float  : " + b);
+        System.out.println("char   : " + c);
+        System.out.println("boolean: " + d);
+        System.out.println("double : " + e);
+        System.out.println("long   : " + f);
+        System.out.println("byte   : " + g);
+        System.out.println("short  : " + h);
+
+        System.out.println("\nWrapper Classes:");
+        System.out.println("Integer  : " + a1);
+        System.out.println("Float    : " + b1);
+        System.out.println("Character: " + c1);
+        System.out.println("Boolean  : " + e1);
+        System.out.println("Double   : " + f1);
+        System.out.println("Long     : " + g1);
+        System.out.println("Byte     : " + h1);
+        System.out.println("Short    : " + i1);
+
     }
 }
