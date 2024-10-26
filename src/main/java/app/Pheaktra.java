b5c08b0c4581b606938d71752d8f5cca0dc606a3
bosnhta <pheaktra0017@gmail.com>
2024-10-26 22:33:33 +0700

    Pheaktra(p6)

--- src/main/java/app/Pheaktra.java
+++ src/main/java/app/Pheaktra.java
@@ -4,20 +4,14 @@ public class
 Pheaktra {
     public static void practise()
     {
-        //p5
-        int a = 15;
-        int b = 4;
-        int addition       = a + b;
-        int subtraction    = a - b;
-        int multiplication = a * b;
-        int division       = a / b;
-        int modulus        = a % b;
-
-        System.out.println("Addition (a + b)      : " + addition);
-        System.out.println("Subtraction (a - b)   : " + subtraction);
-        System.out.println("Multiplication (a * b): " + multiplication);
-        System.out.println("Division (a / b)      : " + division);
-        System.out.println("Modulus (a % b)       : " + modulus);
-
+        //p6
+        int a = 10;
+        int b = 15;
+        System.out.println("a > b  : " + (a > b));
+        System.out.println("a < b  : " + (a < b));
+        System.out.println("a >= b : " + (a >= b));
+        System.out.println("a <= b : " + (a <= b));
+        System.out.println("a == b : " + (a == b));
+        System.out.println("a != b : " + (a != b));
     }
 }
