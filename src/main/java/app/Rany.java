6df29976d5153b7473764a04092216e6b1704583
orn-rany <ranyorn@gmail.com>
2024-10-27 02:10:26 -0700

    add

--- src/main/java/app/Rany.java
+++ src/main/java/app/Rany.java
@@ -2,8 +2,43 @@ package app;
 
 public class Rany {
     public static void practise(){
+//        1
+        int primitiveint=10;
+        float primitivefloat =3.14f;
+        char primitivechar ='A';
+        boolean primitiveboolean =true;
+        double primitivedouble = 9.8165;
+        long primitivelong =1000000000;
+        byte primitivebyte = 20;
+        short primitiveshort = 300;
+        System.out.println("primitive Type:");
+        System.out.println("int="+primitiveint);
+        System.out.println("float="+primitivefloat);
+        System.out.println("char="+primitivechar);
+        System.out.println("boolean="+primitiveboolean);
+        System.out.println("double="+primitivedouble);
+        System.out.println("long="+primitivelong);
+        System.out.println("byte="+primitivebyte);
+        System.out.println("short="+primitiveshort);
+        Integer warpperclassesinteger =15;
+        Float warpperclassesfloat =2.71f;
+        Character warpperclassescharacter ='B';
+        Boolean warpperclasessboolean=false;
+        Double warpperclasessdouble=1.2345;
+        Long warpperclasessLong=2000000000L;
+        Byte wapperclasessbyte=30;
+        Short wapperclasessshort=400;
+        System.out.println("Wapperclasess Clasess:");
+        System.out.println("Integer="+warpperclassesinteger);
+        System.out.println("Float="+warpperclassesfloat);
+        System.out.println("Character="+warpperclassescharacter);
+        System.out.println("Boolean="+warpperclasessboolean);
+        System.out.println("Double="+warpperclasessdouble);
+        System.out.println("Long="+warpperclasessLong);
+        System.out.println("Byte="+wapperclasessbyte);
+        System.out.println("Short="+wapperclasessshort);
 
-        System.out.println("hello rany");
-        System.out.println("hello rany");
+//        2
+
     }
 }
